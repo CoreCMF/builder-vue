6eaa6836083a30dfeff0c699bae84825a9853978
bigrocs <bigrocs@qq.com>
2017-05-20 09:29:39 +0800

    builder-vue init

---
+++ src/components/app/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  created () {
+    this.initMainData()
+  },
+  methods: {
+    initMainData () {
+      this.$http.post(this.$store.state.apiUrl).then((Response) => {
+        console.log(Response.data)
+      })
+    }
+  }
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import App from './components/app'
+import router from './router'
+import store from './store'
+
+export {
+  App,
+  router,
+  store
+}
