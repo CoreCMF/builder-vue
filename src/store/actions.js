916672d0ce952d8bf8c45fbc477c480c4ded8b5b
bigrocs <bigrocs@qq.com>
2017-06-07 14:53:26 +0800

    url to apiurl

--- src/store/actions.js
+++ src/store/actions.js
@@ -8,8 +8,8 @@ export const setMainData = ({ commit,state },mainData) => {
   commit(types.SET_MAIN_DATA,mainData)
 }
 /* 公共post请求方法 */
-export const getData = ({ commit,state },{url = state.apiUrl, postData = '', thenFunction = '', catchFunction = '', notification = true}) => {
-    window.axios.post(url,postData,{withCredentials:true})
+export const getData = ({ commit,state },{apiUrl = state.apiUrl, postData = '', thenFunction = '', catchFunction = '', notification = true}) => {
+    window.axios.post(apiUrl,postData,{withCredentials:true})
     .then((Response) => {
       if (thenFunction) {
         thenFunction(Response)
