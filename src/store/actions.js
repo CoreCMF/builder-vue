e57badf43789e5828eb3cef2c3ca1593ab3e50f3
bigrocs <bigrocs@qq.com>
2017-06-21 08:33:50 +0800

    更改提示现象显示样式to message

--- src/store/actions.js
+++ src/store/actions.js
@@ -7,38 +7,29 @@ import * as types from './mutation-types'
 export const setMainData = ({ commit,state },mainData) => {
   commit(types.SET_MAIN_DATA,mainData)
 }
-export const Notification = (notify, data) => {
-  data.duration = data.duration ? data.duration : 4500;  //设置自动取消时间
-  data.offset = data.offset ? data.offset : 100;  //设置位置偏移
-  notify({
-    title: data.title,
+export const MessageInfo = (message, data) => {
+  message({
     message: data.message,
     type: data.type,
-    iconClass: data.iconClass,
-    customClass: data.customClass,
-    duration: data.duration,
-    onClose: data.onClose,
-    offset: data.offset,
   })
 }
 /* 公共post请求方法 */
-export const getData = ({ commit,state },{apiUrl = state.apiUrl, postData = null, thenFunction = null, catchFunction = null, notify = null}) => {
+export const getData = ({ commit,state },{apiUrl = state.apiUrl, postData = null, thenFunction = null, catchFunction = null, message = null}) => {
     window.axios.post(apiUrl,postData,{withCredentials:true})
     .then((Response) => {
       if (thenFunction) {
         thenFunction(Response)
       }
-      if (notify) {
-        Notification(notify, Response.data)
+      if (message) {
+        MessageInfo(message, Response.data)
       }
     })
     .catch(function (error) {
       if (catchFunction) {
         catchFunction(error)
       }
-      if (notify) {
-        Notification(notify, {
-            title: '操作失败',
+      if (message) {
+        MessageInfo(message, {
             message: '操作失败请联系管理员！',
             type: 'error',
         })
