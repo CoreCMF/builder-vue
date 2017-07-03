31f1a7b375bffd63fd91270d4b8130f76128e8fe
bigrocs <bigrocs@qq.com>
2017-07-03 09:42:48 +0800

    Add getters

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import mutations from './mutations'
 import * as actions from './actions'
+import * as getters from './getters'
 
 Vue.use(Vuex)
 
@@ -27,6 +28,7 @@ const state = {
 }
 export default new Vuex.Store({
   state,
+  getters,
   mutations,
   actions
 })
