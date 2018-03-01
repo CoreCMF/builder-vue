776599d7ae76ec69a0fb385154ae0a8f2c1b6496
bigrocs <bigrocs@qq.com>
2018-03-01 16:46:06 +0800

    允许回执cookies

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ window.axios.defaults.headers.common = {
   'X-CSRF-TOKEN': window.config.csrfToken,
   'X-Requested-With': 'XMLHttpRequest'
 }
+window.axios.defaults.withCredentials=true
 
 export default {
   start (){
